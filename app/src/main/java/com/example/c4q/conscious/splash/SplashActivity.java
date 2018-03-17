4b563f8e1c99e3bdf307e068848377f635e2b7a2
Maria David <mhdavid83@yahoo.com>
2018-03-17 12:47:32 -0400

    updated with splash set up and login screen

---
+++ app/src/main/java/com/example/c4q/conscious/splash/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.c4q.conscious.splash;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.c4q.conscious.LoginActivity;
+import com.example.c4q.conscious.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        TextView conscious_logo = findViewById(R.id.splash_logo);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(1000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+}
