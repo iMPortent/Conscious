0daeba7082c83c0cbcedda4285ccc1e1272ad26a
importent <twill4317@withs.org>
2018-03-18 11:57:12 -0400

    t

--- app/src/main/java/com/example/c4q/conscious/views/activities/MainActivity.java
+++ app/src/main/java/com/example/c4q/conscious/views/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.c4q.conscious;
+package com.example.c4q.conscious.views.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.c4q.conscious.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 
 }
---
+++ app/src/main/java/com/example/c4q/conscious/views/fragments/SmartFragment.java
@@ -0,0 +1,8 @@
+package com.example.c4q.conscious.views.fragments;
+
+/**
+ * Created by c4q on 3/18/18.
+ */
+
+public class SmartFragment {
+}
