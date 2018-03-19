5e97a8b8bd9b81290eec1e71cd5fa17a21ba98ef
Maria David <mhdavid83@yahoo.com>
2018-03-18 23:32:22 -0400

    updated LoinActivity

---
+++ app/src/main/java/com/example/c4q/conscious/views/fragments/SlidingFragment.java
@@ -0,0 +1,11 @@
+package com.example.c4q.conscious.views.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by c4q on 3/18/18.
+ */
+
+public class SlidingFragment extends Fragment {
+
+}
