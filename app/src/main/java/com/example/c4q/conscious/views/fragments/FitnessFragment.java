3a6e0c1178d9abe2f64242fe056b5f2416754b48
importent <twill4317@withs.org>
2018-03-27 21:57:06 -0400

    smart fragment logic for adding goals via edit text

---
+++ app/src/main/java/com/example/c4q/conscious/views/fragments/FitnessFragment.java
@@ -0,0 +1,11 @@
+package com.example.c4q.conscious.views.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by c4q on 3/27/18.
+ */
+
+public class FitnessFragment extends Fragment {
+
+}
