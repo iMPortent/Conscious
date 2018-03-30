ac57ce3426061a8000b9c63ade85d42cba554ca1
Daniel Cortes <danielcortes41@gmail.com>
2018-03-30 01:34:27 -0400

    only needs one Rv

--- app/src/main/java/com/example/c4q/conscious/controller/Intelligent_Tips_Adapter.java
+++ app/src/main/java/com/example/c4q/conscious/controller/Intelligent_Tips_Adapter.java
@@ -1,12 +1,13 @@
-package com.example.c4q.conscious.intelligent_tips_adapters;
+package com.example.c4q.conscious.controller;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class NumberSmartAdapter extends RecyclerView.Adapter<NumberSmartAdapter.ViewHolder> {
+public class Intelligent_Tips_Adapter extends RecyclerView.Adapter<Intelligent_Tips_Adapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
         public ViewHolder(View itemView) {
             super(itemView);
         }
@@ -14,12 +15,12 @@ public class NumberSmartAdapter extends RecyclerView.Adapter<NumberSmartAdapter.
 
 
     @Override
-    public NumberSmartAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public Intelligent_Tips_Adapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(NumberSmartAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(Intelligent_Tips_Adapter.ViewHolder holder, int position) {
 
     }
 
--- app/src/main/java/com/example/c4q/conscious/views/fragments/IntelligentTipsFragment.java
+++ app/src/main/java/com/example/c4q/conscious/views/fragments/IntelligentTipsFragment.java
@@ -11,73 +11,33 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.c4q.conscious.R;
-import com.example.c4q.conscious.intelligent_tips_adapters.BodySmartAdapter;
-import com.example.c4q.conscious.intelligent_tips_adapters.MusicSmartAdapter;
-import com.example.c4q.conscious.intelligent_tips_adapters.NatureSmartAdapter;
-import com.example.c4q.conscious.intelligent_tips_adapters.NumberSmartAdapter;
-import com.example.c4q.conscious.intelligent_tips_adapters.PeopleSmartAdapter;
-import com.example.c4q.conscious.intelligent_tips_adapters.PictureSmartAdapter;
-import com.example.c4q.conscious.intelligent_tips_adapters.SelfSmartAdapter;
-import com.example.c4q.conscious.intelligent_tips_adapters.WordSmartAdapter;
+import com.example.c4q.conscious.controller.Intelligent_Tips_Adapter;
 
 public class IntelligentTipsFragment extends Fragment {
 
 
-    RecyclerView bodySmartRv;
-    RecyclerView musicSmartRv;
-    RecyclerView natureSmartRv;
-    RecyclerView numberSmartRv;
-    RecyclerView peopleSmartRv;
-    RecyclerView pictureSmartRv;
-    RecyclerView selfSmartRv;
-    RecyclerView wordSmartRv;
+    RecyclerView intelligentTipsRv;
+
     Context mContext;
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_goodie_bag, container, false);
+        View root = inflater.inflate(R.layout.fragment_intelligent_tips, container, false);
 
         mContext = getContext();
 
-        bodySmartRv = (RecyclerView) root.findViewById(R.id.body_smart_rv);
-        musicSmartRv = (RecyclerView) root.findViewById(R.id.music_smart_rv);
-        natureSmartRv = (RecyclerView) root.findViewById(R.id.nature_smart_rv);
-        numberSmartRv = (RecyclerView) root.findViewById(R.id.number_smart_rv);
-        peopleSmartRv = (RecyclerView) root.findViewById(R.id.people_smart_rv);
-        pictureSmartRv = (RecyclerView) root.findViewById(R.id.picture_smart_rv);
-        selfSmartRv = (RecyclerView) root.findViewById(R.id.self_smart_rv);
-        wordSmartRv = (RecyclerView) root.findViewById(R.id.word_smart_rv);
+        intelligentTipsRv = (RecyclerView) root.findViewById(R.id.intelligent_tips_rv);
 
-        BodySmartAdapter bodySmartAdapter = new BodySmartAdapter();
-        MusicSmartAdapter musicSmartAdapter = new MusicSmartAdapter();
-        NatureSmartAdapter natureSmartAdapter = new NatureSmartAdapter();
-        NumberSmartAdapter numberSmartAdapter = new NumberSmartAdapter();
-        PeopleSmartAdapter peopleSmartAdapter = new PeopleSmartAdapter();
-        PictureSmartAdapter pictureSmartAdapter = new PictureSmartAdapter();
-        SelfSmartAdapter selfSmartAdapter = new SelfSmartAdapter();
-        WordSmartAdapter wordSmartAdapter = new WordSmartAdapter();
+        Intelligent_Tips_Adapter intelligentTipsAdapter = new Intelligent_Tips_Adapter();
 
-        bodySmartRv.setAdapter(bodySmartAdapter);
-        musicSmartRv.setAdapter(musicSmartAdapter);
-        natureSmartRv.setAdapter(natureSmartAdapter);
-        numberSmartRv.setAdapter(numberSmartAdapter);
-        peopleSmartRv.setAdapter(peopleSmartAdapter);
-        pictureSmartRv.setAdapter(pictureSmartAdapter);
-        selfSmartRv.setAdapter(selfSmartAdapter);
-        wordSmartRv.setAdapter(wordSmartAdapter);
+        intelligentTipsRv.setAdapter(intelligentTipsAdapter);
+
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(mContext, LinearLayoutManager.HORIZONTAL, false);
 
-        bodySmartRv.setLayoutManager(linearLayoutManager);
-        musicSmartRv.setLayoutManager(linearLayoutManager);
-        natureSmartRv.setLayoutManager(linearLayoutManager);
-        numberSmartRv.setLayoutManager(linearLayoutManager);
-        peopleSmartRv.setLayoutManager(linearLayoutManager);
-        pictureSmartRv.setLayoutManager(linearLayoutManager);
-        selfSmartRv.setLayoutManager(linearLayoutManager);
-        wordSmartRv.setLayoutManager(linearLayoutManager);
+        intelligentTipsRv.setLayoutManager(linearLayoutManager);
 
         return root;
     }
